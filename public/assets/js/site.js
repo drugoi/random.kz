dc01e270edc67c571d3d4d9190e8ec01aa0d879c
Nikita Bayev <drugoi.nik@gmail.com>
2013-10-22 17:08:45 +0600

    Test 4

---
+++ public/assets/js/site.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$.getJSON('/assets/words.json', function(data){
+		$('.b-random__word').text(data.words[Math.floor(Math.random() * data.words.length)].w);
+	});
+});
+
+(function() {
+  if (window.pluso)if (typeof window.pluso.start == "function") return;
+  if (window.ifpluso==undefined) { window.ifpluso = 1;
+    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+    s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+    var h=d[g]('body')[0];
+    h.appendChild(s);
+}})();
