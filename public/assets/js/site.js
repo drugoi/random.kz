871527474af09c2a98bea0ba4ad7a457ff8c16ae
Nikita Bayev <drugoi.nik@gmail.com>
2013-10-22 17:15:34 +0600

    Test

--- public/assets/js/site.js
+++ public/assets/js/site.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 	$.getJSON('/assets/js/words.json', function(data){
-		$('.b-random__word').text(data.words[Math.floor(Math.random() * data.words.length)].w);
+		$('.b-random__word').text(data.words[Math.floor(Math.random() * data.words.length)].w).fadeIn();
 	});
 });
