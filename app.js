c303c5e43e3d7663552e3f9e1949943029951935
Nikita Bayev <drugoi.nik@gmail.com>
2013-10-22 15:47:49 +0600

    Test 5

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-var express = require('express');
-var app = express.createServer();
-var port = process.env.PORT || 5000;
+var connect = require('connect');
+var port = process.env.PORT || 8080;
 
-app.set('views', __dirname + '/views');
-app.engine('html', require('ejs').renderFile);
-
-app.get('/', function (req, res) {
-    res.render('index.html');
-});
-
-app.listen(port);
+connect.createServer(
+    connect.static(__dirname);
+).listen(port);
