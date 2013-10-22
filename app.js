43e4fd66763b655c422140f1d2654ce11a0f6d13
Nikita Bayev <drugoi.nik@gmail.com>
2013-10-22 15:58:37 +0600

    Update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var port = process.env.PORT || 8080;
 
-var app = connect().use(connect.static(__dirname + ''));
+var app = connect().use(connect.static(__dirname + '/public'));
 app.listen(port);
