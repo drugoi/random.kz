acfe097dadc7cd5bae7f1a060520fbe9277bd6d7
Nikita Bayev <nikita@bayev.kz>
2013-12-08 02:10:26 +0600

    Updated files

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 var connect = require('connect');
+var fs = require('fs');
 var port = process.env.PORT || 8080;
-
 var app = connect().use(connect.static(__dirname + '/public'));
+
+var fetchData;
+var fetchWord;
+var fetchHtml;
+
+
+fs.readFile('public/assets/js/words.json', handleFile);
+fs.readFile('public/index.html', 'utf-8', indexFile);
+
+function handleFile(err, data) {
+	if (err) throw err;
+    fetchData = JSON.parse(data);
+	fetchWord = fetchData.words[Math.floor(Math.random() * fetchData.words.length)].w;
+}
+
+function indexFile(err, data) {
+	if (err) throw err;
+	fetchHtml = data.toString().replace('{{kazakh_word}}', fetchWord);
+	fs.writeFile('public/index.html', fetchHtml, function(err){
+		if (err) throw err;
+	});
+}
+
 app.listen(port);
