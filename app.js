431b6b72a314a03db7d89cd5bebe2bcc4de5b7b8
Nikita Bayev <nikita@bayev.kz>
2013-12-08 03:51:39 +0600

    Updated functions for best performance [4]

--- app.js
+++ app.js
@@ -11,17 +11,26 @@ var oldHtml;
 function createKzWord() {
 	fetchData = JSON.parse(fs.readFileSync('public/assets/js/words.json', 'utf-8'));
 	fetchWord = '<span class="b-random__word">' + fetchData.words[Math.floor(Math.random() * fetchData.words.length)].w + '</span>';
+	console.log(fetchWord);
 	return fetchWord;
+	next();
 }
 
 function getHtml() {
 	oldHtml = fs.readFileSync('public/_index.html', 'utf-8');
 	newHtml = oldHtml.toString().replace('<span class="b-random__word">{{kazakh_word}}</span>', fetchWord);
+	return newHtml;
+	next();
 }
-app.use(createKzWord);
-app.use(getHtml);
 
-fs.writeFile('public/index.html', newHtml, function (err) {
-	if (err) throw err;
-});
+function createHtml() {
+	fs.writeFile('public/index.html', newHtml, function (err) {
+		if (err) throw err;
+	});
+}
+
+createKzWord();
+getHtml();
+createHtml();
+
 app.listen(port);
