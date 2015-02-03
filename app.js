1d00916bb0f5031eec7528b00a2d7b4de22d250c
Nikita Bayev <drugoi.nik@gmail.com>
2015-02-03 08:49:47 +0600

    Removed compression

--- app.js
+++ app.js
@@ -3,30 +3,52 @@ var http = require('http');
 var serveStatic = require('serve-static');
 var fs = require('fs');
 var compression = require('compression');
+var events = require('events');
+
+var _event = new events.EventEmitter();
 
 var port = process.env.PORT || 8080;
 var app = connect().use(serveStatic(__dirname + '/public'));
-app.use(compression());
 
-var fetchData = function() {
-  fs.readFile('public/assets/js/words.json', 'utf-8', function(err, data) {
-    if (err) throw err;
-    var wordsFile = JSON.parse(data);
-    var word = wordsFile.words[Math.floor(Math.random() * wordsFile.words.length)].w;
+var wordsUtilities = {
+  fetch: function() {
+    fs.readFile('public/assets/js/words.json', 'utf-8', function(err, data) {
+      if (err) throw err;
+      console.info(data);
+      wordsLibrary = JSON.parse(data);
+      return wordsLibrary;
+    });
+  },
+  random: function() {
+    var randomWord = wordsLibrary.words[Math.floor(Math.random() * wordsLibrary.words.length)].w;
+    return randomWord;
+  },
+  render: function() {
     fs.readFile('public/_index.html', 'utf-8', function(err, data) {
       if (err) throw err;
-      var htmlContent = data.replace('{{word}}', word); 
-      fs.writeFile('public/index.html', htmlContent, function(err) {
-        if (err) throw err;
-      });
+      var htmlFile = data;
+      if (!wordsLibrary) {
+        wordsUtilities.fetch();
+        wordsUtilities.compile(wordsUtilities.random(wordsLibrary), htmlFile);
+      } else {
+        wordsUtilities.compile(wordsUtilities.random(wordsLibrary), htmlFile);
+      }
     });
-  });
-};
+  },
+  compile: function(word, htmlFile) {
+    var htmlContent = data.replace('{{word}}', word); 
+    fs.writeFile('public/index.html', htmlContent, function(err) {
+      if (err) throw err;
+    });
+  }
+}
 
-fetchData();
+var wordsLibrary = wordsUtilities.fetch();
+
+wordsUtilities.render();
 
 app.use(function(req, res){
-	fetchData();
+  wordsUtilities.render();
 })
 
 //create node.js http server and listen on port
